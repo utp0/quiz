d86d52f5191d1e7bbae2b324581624d6558b868b
utp0 <paksyh15@gmail.com>
2025-04-17 16:08:36 +0200

    egy alap api request példa

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'angular-project';
+  testMsg: string = "";
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<string>("/api/ping").subscribe(res => {
+      this.testMsg = JSON.stringify(res);
+    })
+  }
 }
