be3c5c5a5dc3cf84134018f4e05eadfd68ac2f20
utp0 <paksyh15@gmail.com>
2025-04-18 19:51:38 +0200

    ejs példa

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const { log } = require("console");
 const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;  // ezt használjuk böngészőben
-const ANGULAR_DEV_SERVER_PORT = 4200;
-const ANGULAR_DEV_SERVER_URL = `http://localhost:${ANGULAR_DEV_SERVER_PORT}`;
+
+app.set("view engine", "ejs");
+app.set("view options", {
+    rmWhitespace: true,
+    async: false,
+});
+app.set("views", path.join(__dirname, "views"));
 
 // teszt
 app.get("/api/ping", (req, res) => {
     res.json({ message: "pong" });
 })
 
-app.use('/', createProxyMiddleware({
-    target: ANGULAR_DEV_SERVER_URL,
-    secure: false,
-    changeOrigin: true,
-    ws: true,
-    logLevel: 'debug',
-}));
+app.get("/", (req, res) => {
+    res.render("main", {
+        page: "partial/homepage",  // ez lenne pl "kerdessor" vagy "quizlista", amik pl a views/partial mappában lennének
+        title: "Quiz - Főoldal",
+        tesztvaltozo: "értékátadás ok"
+    });
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log(`Express: http://127.0.0.1:${PORT}/\n
