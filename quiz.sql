07471e835849c0120e00319c1923c7c7760963dd
Kalman Adam <h373742@stud.u-szeged.hu>
2025-05-17 22:58:00 +0200

    asd

--- quiz.sql
+++ quiz.sql
@@ -241,13 +241,19 @@ BEGIN
 END;
 /
 
+CREATE SEQUENCE seq_kviz_befejezes_log
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
 CREATE OR REPLACE TRIGGER trg_kviz_befejezes_log
 AFTER INSERT ON Eredmeny
 FOR EACH ROW
 BEGIN
     INSERT INTO Kviz_Befejezes_Log (id, felhasznalo_id, kviz_id, pontszam, jatekszoba_id, idobelyeg)
     VALUES (
-        NVL((SELECT MAX(ID)+1 FROM Kviz_Befejezes_Log), 1),
+        seq_kviz_befejezes_log.NEXTVAL,
         :NEW.felhasznalo_id, 
         :NEW.kviz_id, 
         :NEW.pontszam, 
@@ -257,13 +263,19 @@ BEGIN
 END;
 /
 
+CREATE SEQUENCE seq_kviz_letrehozas_log
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
 CREATE OR REPLACE TRIGGER trg_kviz_letrehozas
 AFTER INSERT ON Kviz
 FOR EACH ROW
 BEGIN
     INSERT INTO Kviz_Letrehozas_Log (id, kviz_id, nev)
     VALUES (
-        NVL((SELECT MAX(ID)+1 FROM Kviz_Letrehozas_Log), 1),
+        seq_kviz_letrehozas_log.NEXTVAL,
         :NEW.id, 
         :NEW.nev
     );
